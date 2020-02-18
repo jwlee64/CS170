607604386c74d8e1f066f00464568bbaf36405a3
Josiah Lee <josiahwlee@gmail.com>
2020-02-18 13:27:07 -0800

    print func, goal state

---
+++ project1.py
@@ -0,0 +1,8 @@
+def printPuzzle( puzz, x=3  ):
+	for i in range(x):
+		print (puzz[(x*i)+0], puzz[(x*i)+1], puzz[(x*i)+2])
+
+goal = [1,2,3,4,5,6,7,8,0]
+
+print ("Welcome to 861287993 / jlee434's 8-puzzle solver.")
+
